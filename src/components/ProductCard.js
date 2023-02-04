a368ff95113e5a469794b139715c962da540e8c3
sarwar-asik <shasik2021@gmail.com>
2023-02-04 23:14:53 +0600

    redux cart

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -4,42 +4,57 @@ import { useDispatch } from "react-redux";
 import { useProducts } from "../context/ProductProvider";
 import { actionTypes } from "../state/ProductState/actionTypes";
 import { ADD_TO_CART } from "../redux/actionType/actionType";
-import { addToCart } from "../redux/actionCreator/productyAction";
-
+import {
+  addToCart,
+  removeFromCart,
+} from "../redux/actionCreator/productyAction";
+import { AiOutlineDelete } from "react-icons/ai";
+import { useLocation } from "react-router-dom";
 const ProductCard = ({ product }) => {
- 
-  const dispatch = useDispatch()
-
+  const dispatch = useDispatch();
+  const { pathName } = useLocation();
+console.log(pathName);
   return (
     <div
-      className='shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900'
+      className="shadow-lg rounded-3xl border  p-3 flex flex-col text-indigo-900"
       key={product._id}
     >
-      <div className='h-52 w-52 mx-auto'>
+      <h5 className="text-[18px] font-semibold ">{product._id}</h5>
+      <div className="h-52 w-52 mx-auto">
         <img src={product.image} alt={product.model} />
       </div>
-      <h1 className='font-bold text-center'>{product.model}</h1>
-      <p className='text-center font-semibold mb-3'>Rating: {product.rating}</p>
-      <div className=' flex-1'>
-        <ul className='space-y-2'>
-          {product.keyFeature.map((feature) => {
-            return <li className='text-sm '>{feature}</li>;
+      <h1 className="font-bold text-center">{product.model}</h1>
+      <p className="text-center font-semibold mb-3">Rating: {product.rating}</p>
+      <div className=" flex-1">
+        <ul className="space-y-2">
+          {product?.keyFeature?.map((feature) => {
+            return <li className="text-sm ">{feature}</li>;
           })}
         </ul>
       </div>
-      <div className='flex gap-2 mt-5'>
+      <div className="flex gap-2 mt-5">
+        {pathName?.includes("cart") && (
+          <button
+            onClick={() => dispatch(removeFromCart(product))}
+            className="bg-red-500 rounded-full py-1 px-2 flex items-center gap-4 text-white text-bold"
+          >
+            <AiOutlineDelete />
+          </button>
+        )}
+
+        {!pathName?.includes("cart") && (
+          <button
+            onClick={() => dispatch(addToCart(product))}
+            className="bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold"
+          >
+            Add to cart
+          </button>
+        )}
         <button
-        onClick={()=>dispatch(addToCart(product))}
-          className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
-      
+          title="Add to wishlist"
+          className="bg-indigo-500  py-1 px-2 rounded-full"
         >
-          Add to cart
-        </button>
-        <button
-          title='Add to wishlist'
-          className='bg-indigo-500  py-1 px-2 rounded-full'
-        >
-          <BiListPlus className='text-white' />
+          <BiListPlus className="text-white" />
         </button>
       </div>
     </div>
--- src/redux/reducer/productsReducer.js
+++ src/redux/reducer/productsReducer.js
@@ -1,19 +1,36 @@
 import { ADD_TO_CART, REMOVE_FROM_CART } from "../actionType/actionType";
 
 const initialState ={
-    test:"Test",
-    cart:['']
+cart:[]
 };
 
 const productReducer =(state=initialState,action)=>{
+    const selectedProducts= state.cart.find(
+        product=> product._id === action.payload._id
+    )
+    
+    console.log(selectedProducts);
     switch(action.type){
         case ADD_TO_CART:
+            if(selectedProducts){
+                const newCart = state.cart.filter(
+                    product=>product._id !== selectedProducts._id
+                )
+                selectedProducts.quantity= selectedProducts.quantity +1
+                return {
+                    ...state,
+                    cart:[...newCart,selectedProducts]
+                }
+            }
             return {
                 ...state,
-                cart:[...state.cart,action.payload]
+                cart:[...state.cart,{...action.payload,quantity:1}]
             }
         case REMOVE_FROM_CART:
-            return {}
+            return {
+                ...state,
+                cart :state.cart.filter(product => product._id !== action.payload._id)
+            }
         default:
             return state
     }
