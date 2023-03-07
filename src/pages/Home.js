57e1b0ad54957c3bb655a05c329f01beb0f4cd2b
sarwar-asik <shasik2021@gmail.com>
2023-03-07 12:33:40 +0600

    toolKit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,25 +4,27 @@ import ProductCard from "../components/ProductCard";
 import { useProducts } from "../context/ProductProvider";
 
 const Home = () => {
-  const [products,setProducts]= useState([])
+  const [products, setProducts] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     fetch("products.json")
-    .then(res=>res.json())
-    .then(data=>{
-      // console.log(data);
-      setProducts(data)
-    })
-  },[])
-  const state =useSelector((state)=>state)
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data);
+        setProducts(data);
+      });
+  }, []);
+  const state = useSelector((state) => state);
 
   console.log(state);
-  
+
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10">
       <h1>Our Products</h1>
-      {products?.map(product=>{
-        return <ProductCard key={product.model} product={product}></ProductCard>
+      {products?.map((product) => {
+        return (
+          <ProductCard key={product.model} product={product}></ProductCard>
+        );
       })}
     </div>
   );
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,6 +2,8 @@ import { createStore } from "redux";
 import productReducer from "./reducer/productsReducer";
 import { composeWithDevTools } from "@redux-devtools/extension";
 
-const store = createStore(productReducer,composeWithDevTools()); ////using redux dev tools /////
+const store = createStore(productReducer,composeWithDevTools())
+
+ ////       using redux dev tools/////
 
 export default store;
